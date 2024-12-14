34e376e1fe852d99adbd3fd3a151422765d43122
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-14 10:44:33 -1200

    cross_env init

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,12 @@
 import {app , BrowserWindow} from 'electron'
 import path from 'path'
+import { isDev } from './util.js';
 
 app.on('ready', () => {
     const win = new BrowserWindow({})
+    if (isDev()) {
+        win.loadURL('http://localhost:3000');
+      } else {
     win.loadFile(path.join(app.getAppPath() , 'dist-react/index.html'))
+}
 })
