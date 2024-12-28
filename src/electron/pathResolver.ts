c4f7b4cf8060e907312d47af9bfd193562bdb511
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-28 14:15:53 +0100

    fix : path fixing

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -15,7 +15,15 @@ export function getPreloadPath() {
 export function getUIPath() {
     return path.join(app.getAppPath(), '/dist-react/index.html');
 }
+import { fileURLToPath } from 'url';
 
+// Manually define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export function getAssetPath() {
-    return path.join(app.getAppPath(), isDev() ? '.' : '..', '/src/assets');
+    // Determine the base path depending on the environment
+    const basePath = app.isPackaged
+        ? path.join(process.resourcesPath, 'assets') // For production: resources/assets
+        : path.join(__dirname, '..', 'src', 'assets'); // For development: src/assets
+    return basePath;
 }
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,21 +1,44 @@
 // import { useEffect } from 'react'
 import './App.css'
 import { useStatistics } from './useStatistics';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Chart } from './Chart';
 
 function App() {
   const statistics = useStatistics(20);
+  const [activeView, setActiveView] = useState<View>('CPU');
   const cpuUsages = useMemo(
     () => statistics.map((stat) => stat.cpuUsage),
     [statistics]
   );
+  const ramUsages = useMemo(
+    () => statistics.map((stat) => stat.ramUsage),
+    [statistics]
+  );
+  const storageUsages = useMemo(
+    () => statistics.map((stat) => stat.storageUsage),
+    [statistics]
+  );
+  const activeUsages = useMemo(() => {
+    switch (activeView) {
+      case 'CPU':
+        return cpuUsages;
+      case 'RAM':
+        return ramUsages;
+      case 'STORAGE':
+        return storageUsages;
+    }
+  }, [activeView, cpuUsages, ramUsages, storageUsages]);
+
+  useEffect(() => {
+    return window.electron.subscribeChangeView((view) => setActiveView(view));
+  }, []);
 
   return (
     <>
       <div>
         <div style={{height:140}}>
-            <Chart data={cpuUsages} maxDataPoints={20}/>
+            <Chart data={activeUsages} maxDataPoints={20}/>
         </div>
       </div>
       <h1>Vite + React</h1>
