91538e91233adac893ede6785f31ecbcc505ed6d
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-28 15:10:09 +0100

    fix : more path and resource fixing

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -15,15 +15,8 @@ export function getPreloadPath() {
 export function getUIPath() {
     return path.join(app.getAppPath(), '/dist-react/index.html');
 }
-import { fileURLToPath } from 'url';
 
-// Manually define __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 export function getAssetPath() {
-    // Determine the base path depending on the environment
-    const basePath = app.isPackaged
-        ? path.join(process.resourcesPath, 'assets') // For production: resources/assets
-        : path.join(__dirname, '..', 'src', 'assets'); // For development: src/assets
-    return basePath;
+    console.log(app.getAppPath());
+    return path.join(app.getAppPath(), isDev() ? '..' : '../..', '/src/assets');
 }
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -5,7 +5,7 @@ import { useEffect, useMemo, useState } from 'react';
 import { Chart } from './Chart';
 
 function App() {
-  const statistics = useStatistics(20);
+  const statistics = useStatistics(80);
   const [activeView, setActiveView] = useState<View>('CPU');
   const cpuUsages = useMemo(
     () => statistics.map((stat) => stat.cpuUsage),
@@ -37,7 +37,7 @@ function App() {
   return (
     <>
       <div>
-        <div style={{height:140}}>
+        <div style={{height:140, width: 700}}>
             <Chart data={activeUsages} maxDataPoints={20}/>
         </div>
       </div>
