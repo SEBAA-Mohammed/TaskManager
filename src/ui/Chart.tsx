1d112ae5e7a22235ab6e736892cc9c20b985f2fc
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-12-31 23:30:26 +0100

    feat : add colors to each resource

--- src/ui/Chart.tsx
+++ src/ui/Chart.tsx
@@ -7,26 +7,26 @@ export type ChartProps = {
   selectedView: 'CPU' | 'RAM' | 'STORAGE';
 };
 
-// export const COLOR_MAP = {
-//   CPU: {
-//     stroke: '#5DD4EE',
-//     fill: '#0A4D5C',
-//   },
-//   RAM: {
-//     stroke: '#E99311',
-//     fill: '#5F3C07',
-//   },
-//   STORAGE: {
-//     stroke: '#1ACF4D',
-//     fill: '#0B5B22',
-//   },
-// };
+export const COLOR_MAP = {
+  CPU: {
+    stroke: '#5DD4EE',
+    fill: '#0A4D5C',
+  },
+  RAM: {
+    stroke: '#E99311',
+    fill: '#5F3C07',
+  },
+  STORAGE: {
+    stroke: '#1ACF4D',
+    fill: '#0B5B22',
+  },
+};
 
 export function Chart(props: ChartProps) {
-//   const color = useMemo(
-//     () => COLOR_MAP[props.selectedView],
-//     [props.selectedView]
-//   );
+  const color = useMemo(
+    () => COLOR_MAP[props.selectedView],
+    [props.selectedView]
+  );
   const preparedData = useMemo(() => {
     const points = props.data.map((point) => ({ value: point * 100 }));
     return [
@@ -38,6 +38,6 @@ export function Chart(props: ChartProps) {
   }, [props.data, props.maxDataPoints]);
 
   return (
-    <BaseChart data={preparedData} fill={"0A4D5C"} stroke={"5DD4EE"} />
+    <BaseChart data={preparedData} fill={color.fill} stroke={color.stroke} />
   );
 }
